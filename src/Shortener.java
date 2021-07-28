c39cd5663f6a0fef35683c3d447de7d864d1e379
Aline Korshunova <aline.korshunova00@gmail.com>
2021-07-28 18:07:15 +0300

    Добавлен класс Shortener и методы заглушки getId и getString

---
+++ src/Shortener.java
@@ -0,0 +1,14 @@
+/** Укорачиватель ссылок **/
+
+public class Shortener {
+    /** Возвращает идентификатор id для заданной строки **/
+    public Long getId(String string){
+        return null;
+    }
+
+    /** Возвращает сторку для заданной идентификатора
+     * или null, если передан неверный идентификатор **/
+    public String getString(Long id){
+        return null;
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,7 @@
+/** Точка входа программы **/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
