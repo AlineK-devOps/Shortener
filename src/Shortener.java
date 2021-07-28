be13f4033eef864b1a588b76958017e221fc7316
Aline Korshunova <aline.korshunova00@gmail.com>
2021-07-28 20:13:30 +0300

    Реализованы методы getId и getString

--- src/Shortener.java
+++ src/Shortener.java
@@ -1,14 +1,31 @@
+import strategy.StorageStrategy;
+
 /** Укорачиватель ссылок **/
 
 public class Shortener {
+    private Long lastId = 0L;                /* последнее значение идентификатора, которое было использовано
+                                                при добавлении новой строки в хранилище */
+
+    private StorageStrategy storageStrategy; //стратегия хранения данных
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
+    }
+
     /** Возвращает идентификатор id для заданной строки **/
-    public Long getId(String string){
-        return null;
+    public synchronized Long getId(String string){
+        if (storageStrategy.containsValue(string))
+            return storageStrategy.getKey(string);
+        else{
+            lastId++;
+            storageStrategy.put(lastId, string);
+            return lastId;
+        }
     }
 
     /** Возвращает сторку для заданной идентификатора
      * или null, если передан неверный идентификатор **/
-    public String getString(Long id){
-        return null;
+    public synchronized String getString(Long id){
+        return storageStrategy.getValue(id);
     }
 }
